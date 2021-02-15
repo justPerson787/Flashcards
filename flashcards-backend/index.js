9e0754b98bbf36e76c7b5b239841d0d6529bcae2
N.U <n.n.ushakova@gmail.com>
2021-02-15 18:43:06 -0500

    Connect mongodb

--- flashcards-backend/index.js
+++ flashcards-backend/index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+require('dotenv').config();//to read .env file
+
 const app = express();
 const PORT = 3000;
+
+//Connect to our DB in MongoDB Atlas
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true}
+    );
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log("MongoDB database connection established");
+});
 
 app.get('/', (req, res) => {
     res.send('works!');
