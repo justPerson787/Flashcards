9238e43b08d98f21295440be4798588f3ca4b5db
N.U <n.n.ushakova@gmail.com>
2021-02-19 14:37:12 -0500

    Add EditFlashcard component

--- flashcards-frontend/src/components/views/EditFlashcard.js
+++ flashcards-frontend/src/components/views/EditFlashcard.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { gql, useQuery, useMutation } from '@apollo/client';
+import { toast } from 'react-toastify';
 
-const EditFlashcard = () => (
-    <h1>Edit Flashcard</h1>
-);
+const FLASHCARD_QUERY = gql`
+    query getFlashcard($_id: ID!) {
+        getFlashcard(_id: $_id) {
+            _id
+            question
+            answer
+        }
+    }
+`;
 
+const EDIT_FLASHCARD = gql`
+    mutation updateFlashcard($_id: ID!, $question: String, $answer: String) {
+        updateFlashcard(_id: $_id, input: { question: $question, answer: $answer }) {
+            _id
+            question
+            answer
+        }
+    }
+`;
+
+const EditFlashcard = ({ match, history }) => {
+    const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    const { loading, error, data } = useQuery(FLASHCARD_QUERY, {
+        variables: {
+            _id: match.params.id,
+        },
+    });
+
+    const [updateFlashcard] = useMutation(EDIT_FLASHCARD, { onCompleted() { history.push('/'); } });
+
+    if (loading) return 'Loading...';
+    if (error) return `Error ${error.mesage}`;
+
+    const flashcard = data;
+
+    return (
+        <div className="container mt-3">
+            <h1 className="title">Edit Flashcard</h1>
+        </div>
+    );
+};
+    
 export default EditFlashcard;
